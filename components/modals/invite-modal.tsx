0484b3b1abeabe1672b40fb4e66b99f94d5edadd
rodrigo1995vav <rodrigo1995val@hotmail.com>
2023-12-17 02:06:31 -0300

    Set invite input to read only

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -66,6 +66,7 @@ export const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              readOnly
               disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
               value={inviteUrl}
