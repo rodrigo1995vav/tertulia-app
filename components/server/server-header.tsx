53f76653a48e446009e88e891074b24d331e93cd
rodrigo1995vav <rodrigo1995val@hotmail.com>
2023-12-17 01:52:34 -0300

    Added manage members options and functionalities

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -59,6 +59,15 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Members
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
