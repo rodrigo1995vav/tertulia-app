4df116dbc41b53568fb9490623be39146f68b034
rodrigo1995vav <rodrigo1995val@hotmail.com>
2023-12-17 01:21:07 -0300

    Added edit server modal and functionalities

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,6 +50,15 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
